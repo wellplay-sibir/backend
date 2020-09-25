52096f31ddb5caaa8f7251410777398758012469
DuMoH112 <ew.dim@mail.ru>
2020-09-25 20:44:30 +0000

    Созданы таблицы users и logs_bad_query

---
+++ db_scripts/create_db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE public.version (version bigint DEFAULT 0);
+
+CREATE TABLE logs_bad_query(
+    id bigserial PRIMARY KEY,
+    query text
+);
+
+CREATE TABLE users(
+    id bigserial PRIMARY KEY,
+    number_phone varchar(10) UNIQUE,
+    OTP text,
+    time_create_OTP timestamp without time zone,
+    firstname varchar(255),
+    lastname varchar(255),
+    patronymic varchar(255),
+    photo bytea,
+    id_abs text,
+    role int,
+    status_active boolean DEFAULT true
+);
