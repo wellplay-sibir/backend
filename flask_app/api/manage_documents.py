c2fa380e56700598d20555a52aada3e156470a4f
DuMoH112 <ew.dim@mail.ru>
2020-09-26 12:34:39 +0000

    Реализованы методы free_document, my_documents, change_status

---
+++ flask_app/api/manage_documents.py
@@ -0,0 +1,161 @@
+from flask import Blueprint, jsonify, request
+from psycopg2 import sql
+
+from app.database import Database
+from app.models import check_auth, authorize
+from app.config import config
+
+from api.api import get_status_code
+
+manage_document_bp = Blueprint('manage_document', __name__)
+
+
+@manage_document_bp.route('/free_documents', methods=['GET'])
+def free_document():
+    user = check_auth(request.headers, __name__)
+    if user != True:
+        return user
+    user = authorize.get(request.headers.get('UserToken'))
+    try:
+        database = Database(config)
+    except TypeError:
+        return jsonify({"message": "Нет подключения к БД"})
+    
+    result = []
+
+    query = """
+    SELECT 
+        doc.id,
+        u.lastname,
+        u.firstname,
+        u.patronymic,
+        doc.dt_upload,
+		dt.title
+    FROM documents doc
+    LEFT JOIN document_processing dp ON dp.document_id = doc.id
+    LEFT JOIN users u ON u.id = doc.user_id
+	LEFT JOIN document_type dt ON dt.id = doc.document_type_id
+    WHERE dp.status_code_id is NULL;"""
+
+    for row in database.select_data(query):
+        result.append({
+            "document_id": row[0],
+            "lastname": row[1],
+            "firstname": row[2],
+            "patronymic": row[3],
+            "dt_upload": row[4],
+            "document_type": row[5]
+        })
+        
+    database.close()
+    return jsonify(result)
+
+@manage_document_bp.route('/my_documents', methods=['GET'])
+def my_documents():
+    user = check_auth(request.headers, __name__)
+    if user != True:
+        return user
+    user = authorize.get(request.headers.get('UserToken'))
+    try:
+        database = Database(config)
+    except TypeError:
+        return jsonify({"message": "Нет подключения к БД"})
+    
+    result = []
+
+    query = """
+    SELECT 
+        doc.id,
+        u.lastname,
+        u.firstname,
+        u.patronymic,
+        doc.dt_upload,
+		dt.title
+    FROM documents doc
+    LEFT JOIN document_processing dp ON dp.document_id = doc.id
+    LEFT JOIN users u ON u.id = doc.user_id
+	LEFT JOIN document_type dt ON dt.id = doc.document_type_id
+    WHERE dp.manager_id={};""".format(user.get_id())
+
+    for row in database.select_data(query):
+        result.append({
+            "document_id": row[0],
+            "lastname": row[1],
+            "firstname": row[2],
+            "patronymic": row[3],
+            "dt_upload": row[4],
+            "document_type": row[5]
+        })
+        
+    database.close()
+    return jsonify(result)
+
+@manage_document_bp.route('/change_status', methods=['GET', 'POST'])
+def change_status():
+    user = check_auth(request.headers, __name__)
+    if user != True:
+        return user
+    user = authorize.get(request.headers.get('UserToken'))
+    try:
+        database = Database(config)
+    except TypeError:
+        return jsonify({"message": "Нет подключения к БД"})
+    
+    result = {}
+
+    if request.method == 'GET':
+        return get_status_code(True)
+    elif request.method == 'POST':
+        json = request.get_json(silent=True)
+        if not json:
+            return jsonify({"message": "JSON не найден"}), 400
+        
+        fields = [
+            "document_id",
+            "status_code_id"
+        ]
+        for field in fields:
+            if not json.get(field):
+                return jsonify({"message": f"Поле {field} не найдено"}), 400
+
+        query = None
+        values = None
+        if database.select_data(sql.SQL("SELECT status_code_id FROM document_processing WHERE document_id={document_id}").format(
+            document_id=sql.Literal(json.get("document_id")))
+            ):
+
+            query = "UPDATE {table} SET {fields}={values} WHERE document_id={document_id}"
+
+            values = {
+                "table": sql.Identifier("public", "document_processing"),
+                "fields": sql.SQL(",").join(sql.Identifier(i) for i in [
+                    "status_code_id"
+                ]),
+                "values": sql.SQL(",").join(sql.Literal(i) for i in [
+                    json.get("status_code_id")
+                ]),
+                "document_id": sql.Literal(json.get("document_id"))
+            }
+        else:
+            query = "INSERT INTO {table}({fields}) VALUES({values})"
+
+            values = {
+                "table": sql.Identifier("public", "document_processing"),
+                "fields": sql.SQL(",").join(sql.Identifier(i) for i in [
+                    "document_id",
+                    "status_code_id",
+                    "manager_id"
+                ]),
+                "values": sql.SQL(",").join(sql.Literal(i) for i in [
+                    json.get("document_id"),
+                    json.get("status_code_id"),
+                    user.get_id()
+                ])
+            }
+
+        result = database.insert_data(sql.SQL(query).format(**values))
+
+        
+    database.close()
+    return jsonify(result)
+
--- flask_app/app/router.py
+++ flask_app/app/router.py
@@ -1,6 +1,7 @@
 from app.errorhandler import error_bp
 
 from api.api import api_bp
+from api.manage_documents import manage_document_bp
 
 from personal_area.authorization import auth_bp
 from personal_area.logout import logout_bp
@@ -11,6 +12,7 @@ def route(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(logout_bp)
     app.register_blueprint(error_bp)
+    app.register_blueprint(manage_document_bp)
 
     return True
 
@@ -19,5 +21,6 @@ def csrf_exempt(csrf):
     csrf.exempt(api_bp)
     csrf.exempt(auth_bp)
     csrf.exempt(logout_bp)
+    csrf.exempt(manage_document_bp)
 
     return True
